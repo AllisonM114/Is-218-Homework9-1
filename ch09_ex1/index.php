f5d3225d7e0dd149168a4e02ef40bd5169fc8b0e
allison d manzo <adm52@afsconnect1.njit.edu>
2017-03-21 23:55:28 -0400

    added and edited files for hw

--- ch09_ex1/index.php
+++ ch09_ex1/index.php
@@ -8,35 +8,58 @@ $message = 'Enter some data and click on the Submit button.';
 //process
 $action = filter_input(INPUT_POST, 'action');
 
+//validate and process name
 switch ($action) {
     case 'process_data':
         $name = filter_input(INPUT_POST, 'name');
         $email = filter_input(INPUT_POST, 'email');
         $phone = filter_input(INPUT_POST, 'phone');
+	if (empty($first_name)) {
+	    $message = 'You must enter your first and last name.';
+	    break;
+	}
+	$name = ucwords($name);
+	$i = strpos($name, ' ');
+	if ($i === false) {
+	    $message = 'No space were found in the name.';
+	} else {
+	    $first_name = substr($name, 0, $i);
+	}
 
-        /*************************************************
-         * validate and process the name
-         ************************************************/
-        // 1. make sure the user enters a name
-        // 2. display the name with only the first letter capitalized
+//validate and process the email address
+	if (empty($email)) {
+	    $message = 'You must enter an email address.';
+	    break;
+	}
+	$i = strpos($email, '@');
+	if ($i === false) {
+	    $message = 'No @ found in email address.';
+	    break;
+	}
+	$i = strpos($email, '.');
+	if ($i === false) {
+	    $message = 'No . found in email address.';
+	    break;
+	}
 
-        /*************************************************
-         * validate and process the email address
-         ************************************************/
-        // 1. make sure the user enters an email
-        // 2. make sure the email address has at least one @ sign and one dot character
+//validate and process the phone number
+	$phone = str_replace('(', '', $phone);
+	$phone = str_replace(')', '', $phone);
+	$phone = str_replace('.', '', $phone);
+	$phone = str_replace('-', '', $phone);
+	$phone = str_replace(' ', '', $phone);
+	
+	if (strlen($phone) < 7) {
+	    $message = 'Your phone number must have at least seven digits';
+	    break;
+	}
 
-        /*************************************************
-         * validate and process the phone number
-         ************************************************/
-        // 1. make sure the user enters at least seven digits, not including formatting characters
-        // 2. format the phone number like this 123-4567 or this 123-456-7890
-
-        /*************************************************
-         * Display the validation message
-         ************************************************/
-        $message = "This page is under construction.\n" .
-                   "Please write the code that process the data.";
+//message
+        $message = "Hello $first_name,\n\n"
+                   "Thank you for entering this data:\n\n";
+		   "Name: $name\n"
+		   "Email: $email\n"
+		   "Phone: $phone\n"
 
         break;
 }
